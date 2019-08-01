3f3ff30cb5abc82c62def66124f77fc8dc97a5b5
christopher Njoku <ccn9@njit.edu>
2019-08-01 15:23:32 -0400

    laravel-setup Stage 2

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="/">Learana</a>
+            <a class="navbar-brand" href="/">IS218</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -10,27 +10,11 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item ">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/https://is218a.herokuapp.com">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/discover-online-courses">Discover</a>
-                    </li>
-                    <li class="nav-item">
+                        <a class="nav-link" href="/https://is218a.herokuapp.com/about">About</a>
 
-                        <a class="nav-link" href="/why-learana">Why Learana?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/meet-learana">Meet Learana</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/connect">Connect</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/login">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/get-started">Get Started</a>
-                    </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
                     <input class="form-control mr-sm-2" type="text" placeholder="Search">
